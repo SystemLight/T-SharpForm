38400334b0dd2028ab0c6d194207fab95c82bea1
SystemLight <1466335092@qq.com>
2021-03-05 11:04:00 +0800

    初始化仓库

---
+++ SharpForm/Components/Hello.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace SharpForm.Components
+{
+    public partial class Hello : Component
+    {
+        public Hello() {
+            InitializeComponent();
+        }
+
+        public Hello(IContainer container) {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
